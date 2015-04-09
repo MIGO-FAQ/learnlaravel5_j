ba963d9c10d4a2100f74939ad6e5ab2865ba281c
lalalili <lalalili@gmail.com>
2015-04-09 15:26:20 +0800

    完成教程三 Page 頁面

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,18 +11,23 @@
 |
 */
 
-Route::get('/', 'WelcomeController@index');
+/*Route::get('/', 'WelcomeController@index');
+Route::get('home', 'HomeController@index');*/
 
-Route::get('home', 'HomeController@index');
-
-Route::controllers([
+Route::get('/','HomeController@index');
+Route::get('pages/{id}', 'PagesController@show');
+/*Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
-]);
+]);*/
 
-Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function()
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => 'auth'], function()
 {
-    //Route::get('/', 'AdminHomeController@index');
-    Route::resource('/','AdminHomeController');
+    Route::get('/', 'AdminHomeController@index');
+    //Route::resource('/','AdminHomeController');
     Route::resource('pages', 'PagesController');
 });
